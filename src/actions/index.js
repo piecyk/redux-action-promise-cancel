af04a0e08cc7a05691df81f43d17c9cbf1e389cc
Damian Pieczynski <piecyk@gmail.com>
2018-03-20 06:34:41 +0100

    wip

--- src/actions/index.js
+++ src/actions/index.js
@@ -20,31 +20,47 @@ const defferCancel = deffered => {
   return deffer;
 };
 
-// export const FETCH_QUERY = 'FETCH_QUERY'
+export const FETCH_QUERY = 'FETCH_QUERY';
 export const FETCH_QUERY_PENDING = 'FETCH_QUERY_PENDING';
-export const FETCH_QUERY_CANCELED = 'FETCH_QUERY_CANCELED';
 export const FETCH_QUERY_REJECTED = 'FETCH_QUERY_REJECTED';
 export const FETCH_QUERY_FULFILLED = 'FETCH_QUERY_FULFILLED';
 
 export const fetchQuery = query => {
   return dispatch => {
-    dispatch({type: FETCH_QUERY_PENDING});
     const deffer = defferCancel(fQuery(query));
-    deffer.then(
-      payload => {
-        dispatch({type: FETCH_QUERY_FULFILLED, payload});
-      },
-      error => {
-        dispatch(
-          error.message === PromiseCancelled
-            ? {type: FETCH_QUERY_CANCELED}
-            : {type: FETCH_QUERY_REJECTED, payload: error}
-        );
+    dispatch({
+      type: FETCH_QUERY,
+      payload: deffer,
+    }).catch(error => {
+      if (error.message !== PromiseCancelled) {
+        throw error;
       }
-    );
+    });
     return deffer;
   };
 };
+
+// basic if you need more controllor, alawys you can do
+// export const FETCH_QUERY_CANCELED = 'FETCH_QUERY_CANCELED';
+// export const fetchQuery = query => {
+//   return dispatch => {
+//     dispatch({type: FETCH_QUERY_PENDING});
+//     const deffer = defferCancel(fQuery(query));
+//     deffer.then(
+//       payload => {
+//         dispatch({type: FETCH_QUERY_FULFILLED, payload});
+//       },
+//       error => {
+//         dispatch(
+//           error.message === PromiseCancelled
+//             ? {type: FETCH_QUERY_CANCELED}
+//             : {type: FETCH_QUERY_REJECTED, payload: error}
+//         );
+//       }
+//     );
+//     return deffer;
+//   };
+// };
 
 export const UPDATE_QUERY = 'UPDATE_QUERY';
 export const updateQuery = query => {
